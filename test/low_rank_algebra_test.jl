b207804e400f48e83025b54837dd8186da4e26ac
Flemming Holtorf <holtorf@mit.edu>
2022-02-11 13:29:52 -0500

    rename full to Matrix

--- test/low_rank_algebra_test.jl
+++ test/low_rank_algebra_test.jl
@@ -10,25 +10,36 @@ using LinearAlgebra
     A = U1*Z1'
     lr_A = LowRankApproximation(U1,Z1)
 
-
     B = U2*Z2'
     lr_B = LowRankApproximation(U2,Z2)
 
-    @test norm(full(lr_A + lr_B) - (A + B)) < 1e-10
+    # testing addition
+    @test norm(Matrix(lr_A + lr_B) - (A + B)) < 1e-10
 
     U3, Z3 = randn(1000, 7), randn(128, 7)
     C = U3*Z3'
     lr_C = LowRankApproximation(U3, Z3)
-    @test norm(full(lr_A*lr_C) - A*C) < 1e-10
+    # testing product
+    @test norm(Matrix(lr_A*lr_C) - A*C) < 1e-10
 
     # testing elementwise product
     U4, Z4 = randn(100, 2), randn(1000,2)
     D = U4*Z4'
     lr_D = LowRankApproximation(U4,Z4)
-    @test norm(full(elprod(lr_A, lr_D)) - A .* D) < 1e-10
+    @test norm(Matrix(elprod(lr_A, lr_D)) - A .* D) < 1e-10
 
     # testing elementwise power
     for d in 1:5
-        @test norm(full(elpow(lr_A, d)) - A.^d) < 1e-8
+        @test norm(Matrix(elpow(lr_A, d)) - A.^d) < 1e-8
     end
+
+    # test sum 
+    lr_matrices = [LowRankApproximation(randn(100, 5), randn(100, 5)) for i in 1:5]
+    full_representations = [Matrix(lr_matrix) for lr_matrix in lr_matrices]
+    @test norm(Matrix(sum(lr_matrices)) - sum(full_representations)) < 1e-8
+
+    # test prod
+    lr_matrices = [LowRankApproximation(randn(100, 5), randn(100, 5)) for i in 1:5]
+    full_representations = [Matrix(lr_matrix) for lr_matrix in lr_matrices]    
+    @test norm(Matrix(prod(lr_matrices)) -  prod(full_representations)) < 1e-7
 end
