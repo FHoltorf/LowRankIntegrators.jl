e174fc22643dcdfc6ba524dcc1f7e63fad9b3e51
Flemming Holtorf <holtorf@mit.edu>
2022-02-24 00:57:57 -0500

    updated low rank arithmetic tests with tests for concatenation and indexing

--- test/low_rank_arithmetic_test.jl
+++ test/low_rank_arithmetic_test.jl
@@ -28,11 +28,11 @@ using LinearAlgebra
     U4, Z4 = randn(100, 2), randn(1000,2)
     D = U4*Z4'
     lr_D = TwoFactorApproximation(U4,Z4)
-    @test norm(Matrix(elprod(lr_A, lr_D)) - A .* D) < 1e-10
+    @test norm(Matrix(lr_A .* lr_D) - A .* D) < 1e-10
 
     # testing elementwise power
     for d in 1:5
-        @test norm(Matrix(elpow(lr_A, d)) - A.^d) < 1e-8
+        @test norm(Matrix(lr_A .^ d) - A.^d) < 1e-8
     end
 
     # test sum 
@@ -44,9 +44,32 @@ using LinearAlgebra
     lr_matrices = [TwoFactorApproximation(randn(100, 5), randn(100, 5)) for i in 1:5]
     full_representations = [Matrix(lr_matrix) for lr_matrix in lr_matrices]    
     @test norm(Matrix(prod(lr_matrices)) -  prod(full_representations)) < 1e-5
+
+    # indexing tests
+    A = TwoFactorApproximation([i + j for i in 1:10, j in 1:3], [i + j for i in 1:5, j in 1:3])
+    # individual indexing
+    for i in 1:10, j in 1:5
+        @test A[i, j] == Matrix(A)[i,j]
+    end
+    # slicing
+    for i in 1:10
+        @test all(Matrix(A[i, :]) .== Matrix(A)[[i],:])
+    end
+    for j in 1:5
+        @test all(Matrix(A[:, j]) .== Matrix(A)[:, [j]])
+    end
+    # more indexing
+    @test all(Matrix(A[[1, 3, 7], [5,1]]) .== Matrix(A)[[1,3,7], [5,1]])
+    @test all(Matrix(A[[1, 3, 7], 2]) .== Matrix(A)[[1,3,7], [2]])
+    @test all(Matrix(A[2, [3,2]]) .== Matrix(A)[[2], [3,2]])
+    
+    # concatenation tests
+    B = TwoFactorApproximation([i + j for i in 1:10, j in 1:3], [i + j for i in 1:5, j in 1:3])
+    @test all(Matrix(hcat(A,B)) .== hcat(Matrix(A), Matrix(B)))
+    @test all(Matrix(vcat(A,B)) .== vcat(Matrix(A), Matrix(B)))
 end
 
-@testset "Two Factor Low Rank Arithmetic" begin
+@testset "SVD Like Approximation Arithmetic" begin
     U1, S1, V1 = randn(100, 4), randn(4,4), randn(1000, 4)
 
     U2, S2, V2 = randn(100, 2), randn(2,2), randn(1000, 2)
@@ -74,12 +97,12 @@ end
     U4, S4, V4 = randn(100, 2), randn(2,2), randn(1000,2)
     D = U4*S4*V4'
     lr_D = SVDLikeApproximation(U4,S4,V4)
-    @test norm(Matrix(elprod(lr_A, lr_D)) - A .* D) < 1e-8
+    @test norm(Matrix(lr_A .* lr_D) - A .* D) < 1e-8
 
     # testing elementwise power
     # very lenient test due to poor implementation
     for d in 1:5
-        @test norm(Matrix(elpow(lr_A, d)) - A.^d) < 1e-3
+        @test norm(Matrix(lr_A .^ d) - A.^d) < 1e-3
     end
 
     # test sum 
@@ -89,4 +112,27 @@ end
 
     # test prod
     @test norm(Matrix(prod(lr_matrices)) -  prod(full_representations)) < 1e-5
+
+    # indexing tests
+    A = SVDLikeApproximation([i + j for i in 1:10, j in 1:3], ones(3,3), [i + j for i in 1:5, j in 1:3])
+    # individual indexing
+    for i in 1:10, j in 1:5
+        @test A[i, j] == Matrix(A)[i,j]
+    end
+    # slicing
+    for i in 1:10
+        @test all(Matrix(A[i, :]) .== Matrix(A)[[i],:])
+    end
+    for j in 1:5
+        @test all(Matrix(A[:, j]) .== Matrix(A)[:, [j]])
+    end
+    # more indexing
+    @test all(Matrix(A[[1, 3, 7], [5,1]]) .== Matrix(A)[[1,3,7], [5,1]])
+    @test all(Matrix(A[[1, 3, 7], 2]) .== Matrix(A)[[1,3,7], [2]])
+    @test all(Matrix(A[2, [3,2]]) .== Matrix(A)[[2], [3,2]])
+    
+    # concatenation tests
+    B = SVDLikeApproximation([i + j for i in 1:10, j in 1:3], ones(3,3), [i + j for i in 1:5, j in 1:3])
+    @test all(Matrix(hcat(A,B)) .== hcat(Matrix(A), Matrix(B)))
+    @test all(Matrix(vcat(A,B)) .== vcat(Matrix(A), Matrix(B)))
 end
