796e345913fe2a5ef268a2a724bd0d7790cb5492
Flemming Holtorf <holtorf@mit.edu>
2022-05-23 17:12:59 -0400

    update generic matrix example

--- examples/generic_matrix.jl
+++ examples/generic_matrix.jl
@@ -13,17 +13,17 @@ for k in 1:2
 end
 
 D = Diagonal([2.0^(-j) for j in 1:N])
-Y(t) =  exp(t*Ws[1])*exp(t)*D*exp(t*Ws[2])
+Y(t) = exp(t*Ws[1])*exp(t)*D*exp(t*Ws[2])
 Y0 = Y(0.0)
 Yf = Y(1.0)
 
 # initialization of low rank approximation based on the SVD
 r_range = [4,8,16]
-dt_range = [0.1,0.05,0.01,0.005,0.0001]
+dt_range = [0.1,0.05,0.01,0.005]#,0.0001]
 tol = 1e-8 # needed for rank adaptation
-S_kwargs, K_kwargs, L_kwargs = Dict(:dt => 0.001), Dict(:dt => 0.001), Dict(:dt => 0.001)
+S_kwargs, K_kwargs, L_kwargs = Dict(), Dict(), Dict()
 alg = Euler()
-solvers = [PrimalLieTrotterProjectorSplitting(), StrangProjectorSplitting(), UnconventionalAlgorithm(), RankAdaptiveUnconventionalAlgorithm(tol, r_max = 20)]
+solvers = [ProjectorSplitting(PrimalLieTrotter()), ProjectorSplitting(DualLieTrotter()), ProjectorSplitting(Strang()), UnconventionalAlgorithm(), RankAdaptiveUnconventionalAlgorithm(tol, rmax = 20)]
 error = Dict()
 for r in r_range
     X0 = truncated_svd(Y0, r)
@@ -37,13 +37,14 @@ for r in r_range
 end
 
 # visualization of data
-solver_names = Dict(solvers[1] => "Lie Trotter Projector Splitting", 
-                    solvers[2] => "Strang Projector Splitting",
-                    solvers[3] => "Unconventional Algorithm",
-                    solvers[4] => "Rank Adaptive Unconventional Algorithm")
+solver_names = Dict(solvers[1] => "Primal Lie Trotter Projector Splitting", 
+                    solvers[2] => "Dual Lie Trotter Projector Splitting",
+                    solvers[3] => "Strang Projector Splitting",
+                    solvers[4] => "Unconventional Algorithm",
+                    solvers[5] => "Rank Adaptive Unconventional Algorithm")
 error_comparison = plot(yscale = :log, xscale = :log, xlabel = "step size", ylabel = "error", legend = :bottomleft)
 
-colors = Dict(solvers[1] => :blue, solvers[2] => :red, solvers[3] => :green, solvers[4] => :dodgerblue)
+colors = Dict(solvers[1] => :blue, solvers[2] => :red, solvers[3] => :green, solvers[4] => :dodgerblue, solvers[5] => :orange)
 marker_range = [:v, :hex, :o, :star, :cross, :^]
 markers = Dict(r_range[k] => marker_range[k] for k in 1:length(r_range))
 for r in r_range
