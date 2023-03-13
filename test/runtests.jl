0a0cb3a27903317ab3eaa6b04cbdcea37fb54360
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-03-13 17:04:59 -0400

    debugging index selection errors

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,15 @@
 using LinearAlgebra, LowRankIntegrators
 using Test
 
-include("data_agnostic_approximation.jl")
-include("data_driven_approximation.jl")
-include("data_informed_approximation.jl")
-include("data_agnostic_DEIM_approximation.jl")
-include("sparse_interpolation.jl")
+X = randn(10,10)
+test = svd(X)
+algs = [QDEIM(), DEIM(), LDEIM()]
+for alg in algs
+    idcs = index_selection(test.U[:,1:5], alg)
+    @test length(idcs) == 5
+end
+#include("data_agnostic_approximation.jl")
+#include("data_driven_approximation.jl")
+#include("data_informed_approximation.jl")
+#include("data_agnostic_DEIM_approximation.jl")
+#include("sparse_interpolation.jl")
