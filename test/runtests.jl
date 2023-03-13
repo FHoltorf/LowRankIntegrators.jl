3026ba08f9cfec7cc1cf9058604a581973ee9a55
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-03-13 18:14:15 -0400

    revert to normal tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,8 @@
 using LinearAlgebra, LowRankIntegrators
 using Test
 
-X = randn(10,10)
-test = svd(X)
-algs = [QDEIM(), DEIM(), LDEIM()]
-for alg in algs
-    idcs = index_selection(test.U[:,1:5], alg)
-    @test length(idcs) == 5
-end
-#include("data_agnostic_approximation.jl")
-#include("data_driven_approximation.jl")
-#include("data_informed_approximation.jl")
-#include("data_agnostic_DEIM_approximation.jl")
-#include("sparse_interpolation.jl")
+include("data_agnostic_approximation.jl")
+include("data_driven_approximation.jl")
+include("data_informed_approximation.jl")
+include("data_agnostic_DEIM_approximation.jl")
+include("sparse_interpolation.jl")
