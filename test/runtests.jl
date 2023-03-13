75e56e7e3779f316a2267eaf04c6d2fc921702fe
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-03-13 15:31:13 -0400

    add test for sparse interpolation integrator

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 include("data_agnostic_approximation.jl")
 include("data_driven_approximation.jl")
 include("data_informed_approximation.jl")
+include("data_agnostic_DEIM_approximation.jl")
 include("sparse_interpolation.jl")
