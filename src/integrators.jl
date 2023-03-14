c70a966efa73da0bed9e100da466800d76777bdd
FHoltorf <32248677+FHoltorf@users.noreply.github.com>
2023-03-13 21:26:43 -0400

    restructure filesystem => on the fly interpolation into a separate file/chunk

--- src/integrators.jl
+++ src/integrators.jl
@@ -1,16 +1,5 @@
 # basic catch all cases:
-rank_DEIM(::AbstractDLRAlgorithm_Cache) = -1
-interpolation_indices(::AbstractDLRAlgorithm_Cache) = (Int[], Int[])
-
-struct OnTheFlyInterpolation_Cache
-    params::SparseInterpolation
-    u_prev::SVDLikeRepresentation
-    Π::SparseFunctionInterpolator
-    Π_K::SparseFunctionInterpolator
-    Π_L::SparseFunctionInterpolator
-    Π_S::SparseFunctionInterpolator
-end
-
+include("integrators/on_the_fly_interpolation.jl")
 include("integrators/projector_splitting.jl")
 include("integrators/unconventional.jl")
 include("integrators/data_integrator.jl")
